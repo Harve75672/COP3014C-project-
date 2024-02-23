5d946d173571959fe26465a8fa7e054826e111ae
Briana Harvey <136538630+Harve75672@users.noreply.github.com>
2024-02-23 09:50:00 -0500

    First phase of the code for the addressbook project A few things need to be changed but this is good enough for now.

---
+++ addressbook.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Contact {
+    string name;
+    string address;
+    string phone;
+};
+
+vector<Contact> addressBook;
+
+void addContact() {
+    Contact newContact;
+    cout << "Enter name: ";
+    getline(cin, newContact.name);
+    cout << "Enter address: ";
+    getline(cin, newContact.address);
+    cout << "Enter phone number: ";
+    getline(cin, newContact.phone);
+    addressBook.push_back(newContact);
+    cout << "Contact added 
+    successfully!" << endl;
+}
+
+void displayContacts() {
+    if (addressBook.empty()) {
+        cout << "Address book is empty!" << endl;
+        return;
+    }
+
+    cout << "Contacts in the address book:" << endl;
+    for (int i = 0; i < addressBook.size(); i++) {
+        cout << "Name: " << addressBook[i].name << endl;
+        cout << "Address: " << addressBook[i].address << endl;
+        cout << "Phone: " << addressBook[i].phone << endl;
+        cout << "--------------------------" << endl;
+    }
+}
+
+void searchContact(string name) {
+    bool found = false;
+    for (int i = 0; i < addressBook.size(); i++) {
+        if (addressBook[i].name == name) {
+            cout << "Contact found:" << endl;
+            cout << "Name: " << addressBook[i].name << endl;
+            cout << "Address: " << addressBook[i].address << endl;
+            cout << "Phone: " << addressBook[i].phone << endl;
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        cout << "Contact not found." << endl;
+    }
+}
+
+int main() {
+    int choice;
+    string searchName;
+
+    do {
+        cout << "\nAddress Book Menu\n";
+        cout << "1. Add Contact\n";
+        cout << "2. Display Contacts\n";
+        cout << "3. Search Contact\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        switch(choice) {
+            case 1:
+                addContact();
+                break;
+            case 2:
+                displayContacts();
+                break;
+            case 3:
+                cout << "Enter name to search: ";
+                getline(cin, searchName);
+                searchContact(searchName);
+                break;
+            case 4:
+                cout << "Exiting program. Goodbye!\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice !=4);
+
+    return 0;
+}
